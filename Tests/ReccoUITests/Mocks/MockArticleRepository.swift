dbe48e3c614d01e434c08fc124098561ae5df21d
Roberto Frontado <robertofrontado@gmail.com>
2023-07-20 10:53:54 +0200

    Add ArticleDetailViewModelTest

---
+++ Tests/ReccoUITests/Mocks/MockArticleRepository.swift
@@ -0,0 +1,28 @@
+import XCTest
+@testable import ReccoHeadless
+
+final class MockArticleRepository: ArticleRepository {
+
+    enum ExpectationType {
+        case getArticle
+    }
+    
+    var expectations: [ExpectationType: XCTestExpectation] = [:]
+    var expectedContentId: ContentId?
+
+    var getArticleError: NSError?
+
+    func getArticle(with id: ContentId) async throws -> AppUserArticle {
+        expectations[.getArticle]?.fulfill()
+
+        if let expectedContentId = expectedContentId {
+            XCTAssertEqual(id, expectedContentId)
+        }
+
+        if let getArticleError = getArticleError {
+            throw getArticleError
+        }
+
+        return Mocks.article
+    }
+}
